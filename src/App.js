2ef999f6177a4947b2f7a662a276c68295dd3f39
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-05-19 22:53:36 +0100

    Working on the API calls

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Home from './Home/Home';
 import PageNotFound from './404';
 
 function App() {
+  localStorage.clear();
   const message = {
     packaging: 'We do the packaging',
     seal: 'We seal the package',
